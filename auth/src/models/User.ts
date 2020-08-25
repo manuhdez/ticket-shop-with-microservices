de8ab7bab03ceffaa322468e18d453edeb250c90
Manu Hdez <manu.hdez.dev@gmail.com>
2020-08-26 00:04:26 +0100

    user model setup

---
+++ auth/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+// Describes the data needed to create a new user
+interface UserData {
+  email: string;
+  password: string;
+}
+
+// Describes the properties the User model has
+interface UserModel extends mongoose.Model<UserDocument> {
+  createUser(data: UserData): UserDocument;
+}
+
+// Describes the properties the User document has
+interface UserDocument extends mongoose.Document, UserData {}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model<UserDocument, UserModel>('User', userSchema);
+
+userSchema.statics.createUser = (data: UserData) => {
+  return new User(data);
+};
